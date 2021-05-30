ee8207ee4c0474fc4cfe3e0343d09a5edebb9d8e
xNakero <thenakero@hotmail.com>
2021-05-30 08:34:28 +0200

    added cors to all sources intentionally

--- src/main/java/com/example/meeter/controller/MeetingPlannerController.java
+++ src/main/java/com/example/meeter/controller/MeetingPlannerController.java
@@ -6,13 +6,11 @@ import com.example.meeter.service.impl.MeetingPlannerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin(origins = "**")
 public class MeetingPlannerController {
 
     private final MeetingPlannerServiceImpl meetingPlannerService;
--- src/main/java/com/example/meeter/controller/UserController.java
+++ src/main/java/com/example/meeter/controller/UserController.java
@@ -7,13 +7,11 @@ import com.example.meeter.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("api")
 @RestController
+@CrossOrigin(origins = "**")
 public class UserController {
 
     private final UserServiceImpl userService;
