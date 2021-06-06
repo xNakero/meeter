07b34d8a0682489c8d1ce38788447c04ca4f5a58
xNakero <thenakero@hotmail.com>
2021-06-06 19:19:15 +0200

    Added Access-Control-Allow-Headers to responses.

--- src/main/java/com/example/meeter/config/CorsFilters.java
+++ src/main/java/com/example/meeter/config/CorsFilters.java
@@ -13,6 +13,7 @@ public class CorsFilters implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
         filterChain.doFilter(servletRequest, servletResponse);
     }
 }
